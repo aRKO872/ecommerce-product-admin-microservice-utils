72cb2e005cf04de16f45b79fccc48862744a64db
Arko Antony Mitra <arkoantonymitra@Arko-Antony-Mitra-B661s-MacBook-Pro.local>
2025-09-30 16:47:47 +0530

    added router utils

---
+++ routers/models.go
@@ -0,0 +1,28 @@
+package routers
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
+type ServiceRouter struct {
+	AppID       string
+	Middlewares []mux.MiddlewareFunc
+	Routes      []CommonRouter
+	wg          sync.WaitGroup
+	Shutdown    func() error
+}
+
+type CommonRouter struct {
+	Method      string
+	Endpoint    string
+	Middlewares []mux.MiddlewareFunc
+	Handler     http.HandlerFunc
+}
+
+type httpConfig struct {
+	Port string `env:"PORT" validate:"required,numeric"`
+	Host string `env:"HOST" validate:"required"`
+}
---
+++ routers/router_http.go
@@ -0,0 +1,70 @@
+package routers
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/aRKO872/ecommerce-product-admin-microservice-utils/utils"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+)
+
+func (s *ServiceRouter) ServeHTTP() {
+	var cfg httpConfig
+	if err := utils.ParseEnv(&cfg); err != nil {
+		log.Fatal("Error parsing environment variables: ", err)
+	}
+	validator := validator.New()
+	if err := validator.Struct(cfg); err != nil {
+		log.Fatal("Config validation failed: ", err)
+	}
+
+	router := mux.NewRouter()
+
+	sRouter := router.PathPrefix("/" + s.AppID).Subrouter()
+	for _, cr := range s.Routes {
+		r := sRouter.NewRoute().Subrouter()
+		r.Use(cr.Middlewares...)
+		r.Use(s.Middlewares...)
+		r.HandleFunc(cr.Endpoint, cr.Handler).Methods(cr.Method)
+	}
+
+	srv := s.serveHTTP(cfg, router)
+	s.Shutdown = func() error {
+		return srv.Shutdown(context.Background())
+	}
+
+	go func() {
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+		<-stop
+		log.Printf("Shutting down %s server\n", s.AppID)
+		if err := s.Shutdown(); err != nil {
+			log.Printf("Error shutting down %s server: %v\n", s.AppID, err)
+		}
+	}()
+	
+	s.wg.Wait()
+}
+
+func (s *ServiceRouter) serveHTTP(cfg httpConfig, handler *mux.Router) *http.Server {
+	addr := cfg.Host + ":" + cfg.Port
+	httpServ := &http.Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+		if err := httpServ.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("HTTP server ListenAndServe error: ", err)
+		}
+	}()
+
+	return httpServ
+}
